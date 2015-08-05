ca77748c2f6b75523133e9516ad94888e6a5ddb5
peter <peter@localhost.localdomain>
2015-08-05 15:58:08 +0200

    Still not in working condition...

--- Inc/MPU6050.h
+++ Inc/MPU6050.h
@@ -10,6 +10,57 @@
 
 #include "stm32f1xx_hal.h"
 
+enum MPU_REG {
+	SELF_TEST_X = 13,
+	SELF_TEST_Y = 14,
+	SELF_TEST_Z = 15,
+	SELF_TEST_A = 16,
+	SMPLRT_DIV = 25,
+	CONFIG = 26,
+	GYRO_CONFIG = 27,
+	ACCEL_CONFIG = 28,
+	FIFO_EN = 35,
+	I2C_MST_CTRL = 36,
+	I2C_SLV0_ADDR = 37,
+	INT_PIN_CFG = 55,
+	INT_ENABLE = 56,
+	INT_STATUS = 58,
+	ACCEL_XOUT_H = 59,
+	ACCEL_YOUT_H = 61,
+	ACCEL_ZOUT_H = 63,
+	TEMP_OUT_H = 65,
+	GYRO_XOUT_H = 67,
+	GYRO_YOUT_H = 69,
+	GYRO_ZOUT_H = 71,
+	SIGNAL_PATH_RESET = 104,
+	USER_CTRL = 106,
+	PWR_MGMT_1 = 107,
+	PWR_MGMT_2 = 108,
+	FIFO_COUNTH = 114,
+	FIFO_R_W = 116,
+	WHO_AM_I = 117
+};
+
+enum GYRO_SCALE {
+	GYRO_250DPS, GYRO_500DPS, GYRO_1000DPS, GYRO_2000DPS
+};
+
+enum ACCEL_SCALE {
+	ACCEL_2G, ACCEL_4G, ACCEL_8G, ACCEL_16G
+};
+
+enum MPU_FIFO {
+	FIFO_ACCEL = 1 << 3,
+	FIFO_GYRO_X = 1 << 6,
+	FIFO_GYRO_Y = 1 << 5,
+	FIFO_GYRO_Z = 1 << 4,
+	FIFO_TEMP = 1 << 7
+};
+
+enum MPU_INT {
+	FIFO_OFLOW_EN = 1 << 4, I2C_MST_INT_EN = 1 << 3, DATA_RDY_EN = 1
+};
+
 class MPU6050 {
 	I2C_HandleTypeDef *i2c;
 	float measurement[7];
@@ -26,10 +77,23 @@ public:
 
 private:
 
-	void writeReg(uint8_t reg,uint8_t val);
-	uint8_t readReg(uint8_t reg);
+	void writeReg(MPU_REG reg, uint8_t val);
+	uint8_t readReg(MPU_REG reg);
 
+	uint16_t readReg16(MPU_REG regHigh);
 
+	void setSampleRateDivider(uint8_t div);
+
+	void setDLPF(uint8_t lpf);
+
+	void setGyroScale(GYRO_SCALE scale);
+
+	void setAccelScale(ACCEL_SCALE scale);
+
+	void enableFIFO(MPU_FIFO fifo);
+	void enableFIFO(bool en);
+
+	void enableINT(MPU_INT interrupt);
 
 };
 
--- Src/MPU6050.cpp
+++ Src/MPU6050.cpp
@@ -23,14 +23,56 @@ void MPU6050::getGyro(float* data) {
 void MPU6050::getMeasurement(float* data) {
 }
 
-void MPU6050::writeReg(uint8_t reg, uint8_t val) {
+void MPU6050::writeReg(MPU_REG reg, uint8_t val) {
 	uint8_t buf[2] = { reg, val };
 	HAL_I2C_Master_Transmit(i2c, address, buf, 2, 100);
 }
 
-uint8_t MPU6050::readReg(uint8_t reg) {
+uint8_t MPU6050::readReg(MPU_REG reg) {
 	uint8_t res = 0;
-	HAL_I2C_Master_Transmit(i2c, address, &reg, 1, 100);
+	HAL_I2C_Master_Transmit(i2c, address, (uint8_t*) &reg, 1, 100);
 	HAL_I2C_Master_Receive(i2c, address, &res, 1, 100);
 	return res;
+}
+
+uint16_t MPU6050::readReg16(MPU_REG regHigh) {
+	uint8_t high = readReg(regHigh);
+	uint8_t low = readReg(MPU_REG(regHigh + 1));
+	uint16_t res = (high << 8) | low;
+	return res;
+}
+
+void MPU6050::setSampleRateDivider(uint8_t div) {
+	writeReg(SMPLRT_DIV, div - 1);
+}
+
+void MPU6050::setDLPF(uint8_t lpf) {
+	if (lpf >= 7)
+		lpf = 6;
+	writeReg(CONFIG, lpf);
+}
+
+void MPU6050::setGyroScale(GYRO_SCALE scale) {
+	writeReg(GYRO_CONFIG, scale << 3);
+}
+
+void MPU6050::setAccelScale(ACCEL_SCALE scale) {
+	writeReg(ACCEL_CONFIG, scale << 3);
+}
+
+void MPU6050::enableFIFO(MPU_FIFO fifo) {
+	writeReg(FIFO_EN, fifo);
+}
+
+void MPU6050::enableFIFO(bool en) {
+	uint8_t val;
+	if (en)
+		val = 1;
+	else
+		val = 0;
+	writeReg(USER_CTRL, val << 6);
+}
+
+void MPU6050::enableINT(MPU_INT interrupt) {
+	writeReg(INT_ENABLE, interrupt);
 }
